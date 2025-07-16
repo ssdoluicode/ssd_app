c4d647f439c482d92c6a13cab02ec35f0c9502e4
ssdoluicode <ssdolui.in@gmail.com>
2025-07-17 02:53:14 +0800

    updatee

--- ssd_app/my_custom/report/product_sheet/product_sheet.py
+++ ssd_app/my_custom/report/product_sheet/product_sheet.py
@@ -9,12 +9,26 @@ import frappe
 def execute(filters=None):
 
     query = f"""
-    SELECT cif_p.parent, cif_p.product,cif_p.sc_no, cif_p.qty, cif_p.unit, cif_p.rate AS e_rate, 
-    cif_p.gross AS e_gross, cif_p.charges AS e_charges, cif_p.currency AS e_curr, cif_p.ex_rate AS e_ex_rate,
-    cif_p.gross_usd AS e_gross_usd, cost_p.rate AS i_rate, cost_p.gross AS i_gross, cost_p.charges AS i_charges, 
-    cost_p.currency AS i_curr, cost_p.ex_rate AS i_ex_rate,cost_p.gross_usd AS i_gross_usd 
-    FROM `tabProduct CIF` cif_p LEFT JOIN `tabProduct Cost`cost_p ON cif_p.name=cost_p.id_code
-
+    SELECT 
+    cif_p.parent, 
+    cif_p.product,
+    cif_p.sc_no, 
+    cif_p.qty, 
+    cif_p.unit, 
+    cif_p.rate AS e_rate, 
+    cif_p.gross AS e_gross, 
+    cif_p.charges AS e_charges, 
+    cif_p.currency AS e_curr, 
+    cif_p.ex_rate AS e_ex_rate,
+    cif_p.gross_usd AS e_gross_usd, 
+    cost_p.rate AS i_rate, 
+    cost_p.gross AS i_gross, 
+    cost_p.charges AS i_charges, 
+    cost_p.currency AS i_curr, 
+    cost_p.ex_rate AS i_ex_rate,
+    cost_p.gross_usd AS i_gross_usd 
+	FROM `tabProduct CIF` cif_p 
+	LEFT JOIN `tabProduct Cost` cost_p ON cif_p.name = cost_p.id_code
     """
 
     data = frappe.db.sql(query, as_dict=1)
